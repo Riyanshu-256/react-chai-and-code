776167a797724098ec4835cd9b69cb2bc1063507
Anshu <riyanshu062@gmail.com>
2025-12-14 10:42:16 +0530

    Create basic and vite react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>React Heyyyyy Riyanshu Sharma</h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Vite Heyyyy Riyanshu Sharma</h1>;
+}
+
+export default App;
