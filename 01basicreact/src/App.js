c43ddf3c391301941023cbbb3d2f97054a286e80
Anshu <riyanshu062@gmail.com>
2025-12-14 11:43:29 +0530

    Understanding Structure of React

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,5 +1,12 @@
+import Chai from "./chai";
+
 function App() {
-  return <h1>React Heyyyyy Riyanshu Sharma</h1>;
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <Chai />
+    </>
+  );
 }
 
 export default App;
