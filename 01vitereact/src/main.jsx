5ccf32a62ea8cfeee89ce292c656de9a3612473f
Anshu <riyanshu062@gmail.com>
2025-12-14 13:29:20 +0530

    Updated

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,5 +1,34 @@
-// import { React } from "react";
-import { ReactDOM } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// Simple custom component
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom app</h1>
+    </div>
+  );
+}
+
+// JSX element (most common way)
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">
+//     Visit Google
+//   </a>
+// );
+
+// React.createElement version (what JSX converts into internally)
+// const reactElement = React.createElement(
+//   "a",
+//   { href: "https://google.com", target: "_blank" },
+//   "Click me to visit Google"
+// );
+
+// Render the App component (recommended)
+// You can replace <App /> with <MyApp /> or anotherElement or reactElement
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <MyApp />
+  // anotherElement
+  // reactElement
+);
